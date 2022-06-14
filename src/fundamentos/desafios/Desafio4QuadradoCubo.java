85fc70bb51736abb55d3afc886101723d180cf75
MarinaRuanna <marina_ruana@hotmail.com>
2022-06-14 16:41:13 -0300

    add: modulo de fundmentos

---
+++ src/fundamentos/desafios/Desafio4QuadradoCubo.java
@@ -0,0 +1,25 @@
+package fundamentos.desafios;
+
+/*
+ * Criar um programa que leia um valor e apresente os resultados ao quadrado e ao cubo do valor.
+ */
+
+import java.util.Scanner;
+
+public class Desafio4QuadradoCubo {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite um valor: ");
+        String scan1 = scan.nextLine().replace(",", ".");
+        double num = Double.parseDouble(scan1);
+
+        double quadrado = Math.pow(num, 2);
+        double cubo = Math.pow(num, 3);
+
+        System.out.printf("O quadrado do valor %.2f é %.2f e o seu cubo é %.2f", num, quadrado, cubo);
+
+        scan.close();
+    }
+}
---
+++ src/fundamentos/desafios/Desafio5AreaTriangulo.java
@@ -0,0 +1,29 @@
+package fundamentos.desafios;
+
+/*
+Criar um programa que leia o valor da base e da altura de um triângulo e calcule a área.
+ */
+
+import java.util.Scanner;
+
+public class Desafio5AreaTriangulo {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o vaor da base do triangulo (em cm): ");
+        String scan1 = scan.nextLine().replace(",", ".");
+        double b = Double.parseDouble(scan1);
+
+        System.out.println("Digite a altura do triangulo (em cm): ");
+        String scan2 = scan.nextLine().replace(",", ".");
+        double h = Double.parseDouble(scan2);
+
+        double A = (b * h) / 2;
+
+        System.out.printf("Area do triangulo = %.2fcm²", A);
+
+        scan.close();
+    }
+}
